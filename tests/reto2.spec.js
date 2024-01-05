a23e6e406b291d3a21bcb41942b1d90dde650abd
Leslie-L <leslie.centeno@galileo.edu>
2024-01-05 17:29:17 -0600

    reto 2

---
+++ tests/reto2.spec.js
@@ -0,0 +1,52 @@
+const {test, expect} = require('@playwright/test')
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://playwright.dev/docs/intro');
+});
+
+test('Realizar una busqueda que no tenga resultados', async ({ page }) => {
+  
+  await page.getByLabel('Search').click();
+
+  await page.getByPlaceholder('Search docs').click();
+  await page.getByPlaceholder('Search docs').fill('hascontent');
+
+  await expect(page.locator('.DocSearch-NoResults p')).toBeVisible();
+
+  await expect(page.locator('.DocSearch-NoResults p')).toHaveText('No results for "hascontent"');
+
+})
+
+test('Limpiar el input de busqueda', async ({ page }) => {
+  await page.getByRole('button', { name: 'Search' }).click();
+
+  const searchBox = await page.getByPlaceholder('Search docs');
+
+  await searchBox.click();
+
+  await searchBox.fill('somerandomtext');
+
+  await expect(page.getByPlaceholder('Search docs')).toHaveValue('somerandomtext');
+
+  await page.getByRole('button', { name: 'Clear the query' }).click();
+
+  await expect(searchBox).toHaveAttribute('value', '');
+});
+
+test('Realizar una busqueda que genere al menos tenga un resultado', async ({ page }) => {
+  await page.getByRole('button', { name: 'Search ' }).click();
+
+  const searchBox = page.getByPlaceholder('Search docs');
+
+  await searchBox.click();
+
+  await page.getByPlaceholder('Search docs').fill('havetext');
+
+  expect(searchBox).toHaveValue('havetext');
+
+  // Verity there are sections in the results
+  await page.locator('.DocSearch-Dropdown-Container section').nth(1).waitFor();
+  const numberOfResults = await page.locator('.DocSearch-Dropdown-Container section').count();
+  await expect(numberOfResults).toBeGreaterThan(0);
+
+});
